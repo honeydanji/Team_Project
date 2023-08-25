2da4c2bd224a9157e6a45ffa4df9edd643392d55
suhyun <suhyun5701@gmail.com>
2023-08-25 18:02:36 +0900

    23.08.25_signin,signup

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import Test from './Components/Test';
 import Frontpage from './Components/Frontpage';
 import LogIn from './Components/LogIn';
 import ServiceMain from './Components/ServiceMain';
 import DragDrop from './Components/DragDrop';
 import List from './Components/List';
+import SignUp from './Components/SignUp';
 
 export default function App() {
 
@@ -16,10 +16,10 @@ export default function App() {
         <Routes>
           <Route path='/' element = {<Frontpage />}/>
           <Route path='/servicemain' element = {<ServiceMain />} />
-          <Route path='/test' element = {<Test />} />
           <Route path='/login' element = {<LogIn />} />  
           <Route path='/dragdrop' element = {<DragDrop />}/>   
           <Route path='/list' element = {<List />}/>
+          <Route path='/signup' element = {<SignUp />}/>
         </Routes>
       </main>
     </BrowserRouter>
