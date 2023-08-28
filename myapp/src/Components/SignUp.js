d102c0ce81f01edf8205ebf7ddca41070b562736
suhyun <suhyun5701@gmail.com>
2023-08-28 12:12:36 +0900

    23.08.28 feat:  회원가입 시 DB에 정보 저장
    Sign up에서 restAPI로 flask로 전달 후 회원가입 정보 DB 저장

--- myapp/src/Components/SignUp.js
+++ myapp/src/Components/SignUp.js
@@ -1,7 +1,35 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faBuilding, faLock, faEnvelope, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignUp() {
+
+    //회원가입 정보
+    const [name, setName] = useState('');
+    const [login_email, setLoginEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirm_password, setConfirmPassword] = useState('');
+    const [phone_number, setPhoneNumber] = useState('');
+    const [company_name, setCompanyName] = useState('');
+
+    const handleSignupButtonClick = () => {
+        const data = {
+            name,
+            login_email,
+            password,
+            confirm_password,
+            phone_number,
+            company_name
+        };
+        axios.post('http://10.125.121.183/register', data)
+            .then((res) => {
+                alert("회원가입 성공")
+            })
+            .catch((err) => {
+                alert("회원가입 시 문제 발생")
+            });
+    };
 
     return (
         <section className="bg-white dark:bg-gray-900">
@@ -26,7 +54,7 @@ export default function SignUp() {
                             <FontAwesomeIcon icon={faUser} style={{ color: '#888', position: 'relative', left: '15px' }} />
                         </span>
 
-                        <input type="text" class="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Username" />
+                        <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Username" onChange={(e) => setName(e.target.value)} required/>
                     </div>
 
                     <div className="relative flex items-center mt-6">
@@ -34,7 +62,7 @@ export default function SignUp() {
                             <FontAwesomeIcon icon={faEnvelope} style={{ color: '#888', position: 'relative', left: '15px' }} />
                         </span>
 
-                        <input type="email" class="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address" />
+                        <input type="email" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address" onChange={(e) => setLoginEmail(e.target.value)} required/>
                     </div>
 
                     <div className="relative flex items-center mt-4">
@@ -42,7 +70,7 @@ export default function SignUp() {
                             <FontAwesomeIcon icon={faLock} style={{ color: '#888', position: 'relative', left: '15px' }} />
                         </span>
 
-                        <input type="password" className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Password" />
+                        <input type="password" className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required/>
                     </div>
 
                     <div className="relative flex items-center mt-4">
@@ -50,25 +78,25 @@ export default function SignUp() {
                             <FontAwesomeIcon icon={faLock} style={{ color: '#888', position: 'relative', left: '15px' }} />
                         </span>
 
-                        <input type="password" className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Confirm Password" />
+                        <input type="password" className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} required/>
                     </div>
                     <div className="relative flex items-center mt-6">
                         <span className="absolute">
                             <FontAwesomeIcon icon={faPhone} style={{ color: '#888', position: 'relative', left: '15px' }} />
                         </span>
 
-                        <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Phone Number" />
+                        <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Phone Number" onChange={(e) => setPhoneNumber(e.target.value)} required/>
                     </div>
                     <div className="relative flex items-center mt-6">
                         <span className="absolute">
                             <FontAwesomeIcon icon={faBuilding} style={{ color: '#888', position: 'relative', left: '15px' }} />
                         </span>
 
-                        <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Company Name" />
+                        <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Company Name" onChange={(e) => setCompanyName(e.target.value)}/>
                     </div>
 
                     <div className="mt-6">
-                        <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                        <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50" onClick={handleSignupButtonClick}>
                             Sign Up
                         </button>
 
