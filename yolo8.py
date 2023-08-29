3a697ebba4e4ca7e0f457c4c4fbf8d13d4201c18
jwjb1020 <jwjb1020@gmail.com>
2023-08-29 17:51:42 +0900

    Feat: back에서 이미지 받아서 결과 보여주는 것 성공 - flask를 사용해서 api받아옴 - 나온 클래스, 정확도, x,y좌표 등 정보를 json 파일로 전달 확인 - 이제 이미지 내보내는 코드 작성해야함 -ply받는 코드도 작성해야함

--- yolo8.py
+++ yolo8.py
@@ -1,39 +1,72 @@
 from flask import Flask, request, jsonify
 from ultralytics import YOLO
 from PIL import Image
+from flask_cors import CORS
+import numpy as np
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+CORS(app)
 
+# 모델 초기화
 model = YOLO("model_v3.pt")
 
-@app.route("/predict", methods=["POST"])
+@app.route("/uploadFlask", methods=["POST"])
 def home():
-    if "image" not in request.files:
-        return "No image part"
+    try:
+        if "imageFile" not in request.files:
+            return "No image part"
 
-    image = request.files["image"]
-    if image.filename == "":
-        return "No selected file"
-
-    img = Image.open(image)
-    
-    # 모델로 이미지 예측을 수행합니다.
-    pred_results = model.predict(source=img, save=True, conf=0.6, save_txt=True, device="cpu", project="results", name="jwj")
-   
-    
-
-
-    # 결과가 리스트이므로 첫 번째 요소에서 예측 결과를 추출하여 JSON으로 가공합니다.
-    if pred_results and len(pred_results) > 0:
-        pred_result = pred_results[0]
-        predictions = []
-        for pred in pred_result.pred[0]:
-            label, confidence, box = pred.tolist()
-            predictions.append({"label": label, "confidence": confidence, "box": box})
+        image = request.files["imageFile"]
+        if image.filename == "":
+            return "No selected file"
+                
+        img = Image.open(image)
+        # 이미지를 임시 파일로 저장합니다.
+        temp_image_path = "temp_image.jpg"
+        img.save(temp_image_path)
+        # 모델로 이미지 예측 수행
+        # results = model(temp_image_path, conf=0.6, save_txt=True, device="cpu",save=True, project="results",name="output")/
+        results = model(temp_image_path, conf=0.6, device="cpu",project="results",name="output")
         
-        return jsonify({"predictions": predictions})
-    else:
-        return "No predictions available"
+         # 임시 이미지 파일을 삭제합니다.
+        os.remove(temp_image_path)
+        
+        response_data = []  # 응답 데이터를 담을 리스트 초기화
+
+        for result in results:
+            # 탐지된 객체 정보 추출
+            # 모두 tesor 타입이라서 json에서 읽을 수 있는 형태로 전처리 해야함
+            class_name = result.boxes.cls.tolist()
+            accuracy = result.boxes.conf.tolist()
+            xy_list = result.masks.xyn  
+        
+        
+        for index, (coords, class_val, acc_val) in enumerate(zip(xy_list, class_name, accuracy)):
+            x_coords = [coord[0].item() for coord in coords]
+            y_coords = [coord[1].item() for coord in coords]
+    
+            print(f"Object {index} - Class: {class_val}, Accuracy: {acc_val}, x coordinates: {x_coords} ,y coordinates: {y_coords}")
+            
+            # 응답 데이터를 딕셔너리로 구성하여 리스트에 추가
+            object_data = {
+                "Object_num" : index,
+                "class_name": class_val,
+                "accuracy": acc_val,
+                "x_coordinates": x_coords,
+                "y_coordinates": y_coords
+            }
+            response_data.append(object_data)
+           
+
+        # JSON 형식으로 응답 데이터 전송
+        return jsonify(response_data)
+
+    except Exception as e:
+        # 예외가 발생한 경우에 실행될 코드
+        app.logger.error(f"Error: {str(e)}")
+        return f"Error: {str(e)}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
