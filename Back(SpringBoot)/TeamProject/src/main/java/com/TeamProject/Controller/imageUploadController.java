f43a96d878fa6de50b23fba1a0a1486197b5b58e
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-08-29 17:34:54 +0900

    # imageUpload(server)
    클라이언트가 요청을 보내면 데이터를 local에 저장한다. 후에 다시 원본데이터를 외부 api를 통해서 전송한다.
    DB에는 받은 이미지를 url로 저장한다.

--- Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Controller/imageUploadController.java
+++ Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Controller/imageUploadController.java
@@ -1,25 +1,62 @@
 package com.TeamProject.Controller;
 
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.TeamProject.Service.imageUploadService;
+import com.TeamProject.Service.FlaskService.imageSendService;
+import com.TeamProject.Service.SpringBootService.imageUploadService;
 
 @RestController
 public class imageUploadController {
 
     @Autowired
     imageUploadService imageuploadservice;
-    
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadController(@RequestParam(name = "imageFile", required = false) MultipartFile imageFile) {
-        imageuploadservice.uploadService(imageFile);
-        return ResponseEntity.ok("성공?");
+    @Autowired
+    imageSendService imagesendservice;
+    
+    @PostMapping("/uploadSpring")
+    public ResponseEntity<String> uploadController(@RequestParam(name = "pngFile", required = false) MultipartFile pngFile,
+                                                   @RequestParam(name = "plyFile", required = false) MultipartFile plyFile) {
+        imageuploadservice.uploadService(pngFile, plyFile); // StringBoot 
+        imagesendservice.sendImage(pngFile, plyFile); // Flask
+        return ResponseEntity.ok("SpringBoot 이미지 전송 성공");
     }
 
+    // 이미지 파일이 저장된 디렉토리 경로를 설정.
+	private final String imageDirectory = "C:/Team_Project/Back(SpringBoot)/TeamProject/image";
+
+	// 이미지 조회
+	@GetMapping("/upload/image/{imageName:.+}")
+	public ResponseEntity<Resource> getImage(@PathVariable String imageName) throws MalformedURLException {
+
+        // 요청된 이미지 파일 이름을 사용하여 이미지 파일의 경로를 가져오기.
+        Path imagePath = Paths.get(imageDirectory).resolve(imageName);
+        Resource imageResource = new UrlResource(imagePath.toUri());
+
+        // 이미지 파일이 존재하지 않는 경우.
+        if (!imageResource.exists()) {
+            // 이미지가 없을 경우에 대한 처리를 여기에 작성.
+            return ResponseEntity.notFound().build();
+        }
+
+        // 이미지 파일을 응답으로 반환합니다.
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .contentType(MediaType.IMAGE_PNG)
+                .body(imageResource);
+    }
 }
---
+++ Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Service/SpringBootService/imageUploadService.java
@@ -0,0 +1,75 @@
+package com.TeamProject.Service.SpringBootService;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.TeamProject.Domain.twoOriginalImage;
+import com.TeamProject.Repository.imageUploadRepository;
+
+
+
+@Service
+public class imageUploadService {
+
+    @Autowired
+    imageUploadRepository imageuploadrepository;
+
+    // 이미지 파일의 기본 URL
+	//private final String imageBaseURL = "http://10.125.121.183:8080/upload/image/"; // 클라이언트 실행
+    private final String imageBaseURL = "http://localhost:8080/upload/image/"; // 로컬 실행
+
+
+    public ResponseEntity<String> uploadService(MultipartFile pngFile, MultipartFile plyFile) {
+
+        twoOriginalImage twooriginalimage = new twoOriginalImage();
+
+        // 2d, 3d
+        if (pngFile != null && !pngFile.isEmpty()) {
+		    try {
+		        String[] imageFileName = saveImage(pngFile, plyFile);
+		        twooriginalimage.setTwoOriginalPath(imageBaseURL + imageFileName[0]); // 2d 이미지 파일 이름 설정
+                // threeoriginalpointcloud.setThreeOriginalPointCloud(imageBaseURL + imageFileName[1]); 3d 이미지 파일 이름 설정
+            } catch (IOException e) {
+		        e.printStackTrace();
+		    }
+		}
+		imageuploadrepository.save(twooriginalimage);
+        return null;
+    }
+
+    // 이미지 저장 메서드
+    private String[] saveImage(MultipartFile pngFile, MultipartFile plyFile) throws IOException {
+    	if (!pngFile.isEmpty() && !plyFile.isEmpty()) {
+            try {
+                // 원본 파일 이름 가져오기
+                String original2D = pngFile.getOriginalFilename();
+                String original3D = plyFile.getOriginalFilename();
+
+                // 파일 저장 경로 설정 (필요에 따라 변경해주세요)
+                String savePath2D = "C:/Team_Project/Back(SpringBoot)/TeamProject/image/2D/";
+                String savePath3D = "C:/Team_Project/Back(SpringBoot)/TeamProject/image/3D/";
+
+                // 저장할 파일 객체 생성
+                File savedFile2D = new File(savePath2D + original2D);
+                File savedFile3D = new File(savePath3D + original3D);
+
+                // MultipartFile의 내용을 파일에 저장
+                pngFile.transferTo(savedFile2D);
+                plyFile.transferTo(savedFile3D);
+
+                String[] originalFile = new String[] {original2D, original3D};
+        
+				return originalFile;
+            } catch (Exception e) {
+                e.printStackTrace();
+                // 예외 발생 시 null이나 다른 값을 반환하거나 처리하는 등의 방법 선택
+            }
+        }
+        return null; // 파일이나 이미지가 없을 경우
+    }
+}
