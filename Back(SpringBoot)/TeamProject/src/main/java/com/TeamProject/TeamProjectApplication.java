a933dd43d91bde96b5b1a116ad958c442a54312b
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-08-29 17:30:03 +0900

    # App

---
+++ Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/TeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.TeamProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectApplication.class, args);
+	}
+
+}
