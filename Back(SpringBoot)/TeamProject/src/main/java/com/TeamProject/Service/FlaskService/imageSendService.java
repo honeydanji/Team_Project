32c5b0173c0c9c8c289b746ba79cfe07ba82beba
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-08-29 17:27:29 +0900

    # image 전송 (외부 API)
    외부 API로 데이터 요청 및 응답을 위해 spring3에서 제공하는 RestTemplate를 사용했다. 사용법이 익숙치 않아서 구글링을 통해 주석을
    적어가면 사용했다. 작동원리에 대해서는 알려고 노력했다.  서버에서 외부 API와 통신하는 건 처음이었는데 생각보다 복잡하지 않고
    REST FUL 방식을 사용한 경험이 있어 빠르게 진행 되었다.  응답으로는 flask에서 처리한 데이터 결과값을 json형태로
    받아서 DB에 저장할 것이다.

---
+++ Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Service/FlaskService/imageSendService.java
@@ -0,0 +1,69 @@
+package com.TeamProject.Service.FlaskService;
+
+import java.io.File;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class imageSendService {
+    
+    public void sendImage(MultipartFile pngFile, MultipartFile plyFile) {
+        // 로컬 이미지 파일을 Flask 서버로 전송하는 로직 구현
+
+        // Flask 서버의 엔드포인트 URL
+        String flaskServerUrl = "http://10.125.121.180:80/uploadFlask";
+
+        // 이미지 파일의 이름 가져오기(타입 변환)
+        String twoImageName = pngFile.getOriginalFilename();
+        String threeImageName = plyFile.getOriginalFilename();
+
+        // 전송하고자 하는 로컬 파일의 경로 (변경 가능)
+        String localFilePath2D = "C:/Team_Project/Back(SpringBoot)/TeamProject/image/2D/" + twoImageName;
+        String localFilePath3D = "C:/Team_Project/Back(SpringBoot)/TeamProject/image/3D/" + threeImageName;
+
+        // HTTP POST 요청 생성
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 파일 전송을 위한 MultiValueMap 설정
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+
+        // 로컬 파일을 파일 리소스로 변환하여 첨부
+        File localFile2d = new File(localFilePath2D);
+        File localFile3d = new File(localFilePath3D);
+
+        // 2d, 3d map 추가
+        map.add("pngFile", new FileSystemResource(localFile2d));
+        map.add("pngFile", new FileSystemResource(localFile3d));
+
+
+        // HTTP Headers 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
+
+        // HTTP POST 요청 전송 및 응답 처리(응답 객체)
+        ResponseEntity<String> response = restTemplate.exchange(flaskServerUrl, HttpMethod.POST, requestEntity, String.class);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            System.out.println("로컬 이미지 전송 성공");
+
+             // 받아온 응답 데이터 확인
+            String responseBody = response.getBody();
+            System.out.println("Response from Flask: " + responseBody);
+        } else {
+            System.out.println("로컬 이미지 전송 실패: " + response.getStatusCode());
+            // 실패 시 예외 처리 또는 로그 등 추가 작업 수행
+        }
+    }
+}
