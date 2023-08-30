88dc80283d75e8df9f5c098d0c299650e9306ca8
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-08-30 17:36:15 +0900

    # ~ing

---
+++ Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Controller/SpringBootController/twoSegmentationImageController.java
@@ -0,0 +1,9 @@
+package com.TeamProject.Controller.SpringBootController;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequiredArgsConstructor
+public class twoSegmentationImageController {
+    
+}
