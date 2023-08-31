b23d6516e4f1f8f15cdb61c5a008ef0071d9a9f5
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-08-31 17:51:20 +0900

    # 수정
    - POST > GET

--- Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Controller/imageUploadController.java
+++ Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Controller/imageUploadController.java
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,7 +31,7 @@ public class imageUploadController {
 
     private final historyTableService historytableservice; 
     
-    @PostMapping("/uploadSpring")
+    @GetMapping("/uploadSpring")
     public ResponseEntity<String> uploadController(@RequestParam(name = "pngFile", required = false) MultipartFile pngFile,
                                                    @RequestParam(name = "plyFile", required = false) MultipartFile plyFile) {
 
@@ -64,7 +63,7 @@ public class imageUploadController {
 
         imageuploadservice.uploadService(pngFile, plyFile, history); // StringBoot 
         imagesendservice.sendImage(pngFile, plyFile, history); // Flask
-        //return ResponseEntity.ok("/upload/image/" + pngFile.getOriginalFilename());
+        //return ResponseEntity.ok("http://10.125.121.183:8080/upload/image/" + pngFile.getOriginalFilename());
         return ResponseEntity.ok(pngFile.getOriginalFilename());
         //return ResponseEntity.ok(date); // 이미지 업로드 날짜 반환       
     }
