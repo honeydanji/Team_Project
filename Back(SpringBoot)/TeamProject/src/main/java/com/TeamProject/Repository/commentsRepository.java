7abcbee6bbee2780cab6bd4bfb70ca3106f263f7
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-09-07 18:01:24 +0900

    # comments
    - @transaction 추가
    - put, delete 추가

--- Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Repository/commentsRepository.java
+++ Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Repository/commentsRepository.java
@@ -9,6 +9,9 @@ import com.TeamProject.Domain.historyTable;
 public interface commentsRepository extends JpaRepository<comments, Integer> {
 
     @Query("SELECT c.contents FROM comments c WHERE historyId = ?1")
-    String contentsByHistoryId(historyTable his);
-    
+    String contentsByHistoryId(historyTable historyId);
+
+    comments findByHistoryId(historyTable historyId);
+
+    void deleteByHistoryId(historyTable historyId);
 }
--- Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Service/SpringBootService/commentsService.java
+++ Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Service/SpringBootService/commentsService.java
@@ -11,6 +11,7 @@ import com.TeamProject.Repository.commentsRepository;
 import com.TeamProject.Repository.historyTableRepository;
 import com.TeamProject.Repository.membersRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -21,7 +22,8 @@ public class commentsService {
     private final historyTableRepository historytablerepository; // 히스토리테이블
     private final membersRepository membersrepository; // 멤버테이블
 
-    public ResponseEntity<String> commentUpload(int historyId, String comment, Authentication authentication) {
+    @Transactional
+    public ResponseEntity<String> commentUpload(int historyId, String contents, Authentication authentication) {
 
         String userEmail = authentication.getName(); // 로그인 ID
 
@@ -32,16 +34,64 @@ public class commentsService {
         if (userEmail == null) {
             return ResponseEntity.ok("권한이 없습니다.");
         }else if(hisId.equals(memId)) {
+
+            if(commentsrepository.findAll() != null) {
+                return ResponseEntity.ok("코멘트는 1개만 작성가능.");
+            }
+
             historyTable his = historytablerepository.findByHistoryId(historyId);
 
             comments com = new comments();
 
-            com.setContents(comment);
+            com.setContents(contents);
             com.setHistoryId(his);
 
             commentsrepository.save(com);
 
             return ResponseEntity.ok("글 작성이 완료되었습니다.");
+        }else{
+            return null;
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<String> commentUpdate(int historyId, String contents, Authentication authentication) {
+
+        String userEmail = authentication.getName(); // 로그인 ID
+
+        // 히스토리아이디로 가져온 userId와 실제 userId가 일치하는 지 확인
+        members hisId = historytablerepository.findByHistoryId(historyId).getUserId();
+        members memId = membersrepository.findByloginEmail(userEmail); 
+
+        if (userEmail == null) {
+            return ResponseEntity.ok("권한이 없습니다.");
+        }else if(hisId.equals(memId)) {
+            comments com = commentsrepository.findByHistoryId(historytablerepository.findByHistoryId(historyId));
+
+            com.setContents(contents);
+
+            commentsrepository.save(com);
+
+            return ResponseEntity.ok("글 수정이 완료되었습니다.");
+        }else{
+            return null;
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<String> commentDelete(int historyId, Authentication authentication) {
+
+        String userEmail = authentication.getName(); // 로그인 ID
+
+        // 히스토리아이디로 가져온 userId와 실제 userId가 일치하는 지 확인
+        members hisId = historytablerepository.findByHistoryId(historyId).getUserId();
+        members memId = membersrepository.findByloginEmail(userEmail); 
+
+        if (userEmail == null) {
+            return ResponseEntity.ok("권한이 없습니다.");
+        }else if(hisId.equals(memId)) {
+            commentsrepository.deleteByHistoryId(historytablerepository.findByHistoryId(historyId));
+            return ResponseEntity.ok("글 삭제가 완료되었습니다.");
         }else{
             return null;
         }
