7c69dcc73ac26be397df82c1dbde6c46fc756454
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-09-07 17:33:44 +0900

    # delete

--- Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Controller/SpringBootController/twoSegmentationImageController.java
+++
@@ -1,9 +0,0 @@
-package com.TeamProject.Controller.SpringBootController;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-//@RequiredArgsConstructor
-public class twoSegmentationImageController {
-    
-}
