5202d61d57870370932ed866db1ea094d62e33d0
honeydanji <tjdwls3680@naver.com>
2023-10-16 12:33:39 +0900

    # + ControllerTestCode
    - 단순 수정

--- Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Dto/membersDTO.java
+++ Back(SpringBoot)/TeamProject/src/main/java/com/TeamProject/Dto/membersDTO.java
@@ -1,7 +1,5 @@
 package com.TeamProject.Dto;
 
-import com.TeamProject.Validation.MemberValidation.EmailValidationGroups;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
