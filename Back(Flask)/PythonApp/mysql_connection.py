29b26760167eb6934d6bbb3765769fa8f590e4ac
honeydanji <tjdwls3680@naver.com>
2023-12-17 21:37:08 +0900

    DELETE : 파일 정리

--- Back(Flask)/PythonApp/mysql_connection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection() :
-    connection = mysql.connector.connect(
-        host='localhost',
-        database='k3',
-        user='root',
-        password='1234')
-    return connection
